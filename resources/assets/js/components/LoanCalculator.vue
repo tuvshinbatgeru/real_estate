50d8cb836a2c1ea94dd29b427ea289af8591e5c8
Tuvshinbat <gan.tuvshinbat@gmail.com>
2019-07-26 09:28:14 +0800

    .

--- resources/assets/js/components/LoanCalculator.vue
+++ resources/assets/js/components/LoanCalculator.vue
@@ -3,7 +3,7 @@
       <thead>
         <tr>
           <th scope="col"></th>
-          <th scope="col">Төгрөг</th>
+          <th scope="col"></th>
         </tr>
       </thead>
       <tbody>
@@ -18,28 +18,24 @@
           </td>
         </tr>
         <tr>
-          <th scope="row">Үндсэн үнэ</th>
-          <td>{{ currency(amount) }}</td>
+          <th scope="row" style="vertical-align: middle;">Үндсэн үнэ</th>
+          <td><input class="form-control" type='number' v-model="amount" @change="onAmountChange($event)" aria-describedby="emailHelp" placeholder="оруулах" /></td>
         </tr>
         <tr>
-          <th scope="row">Урьдчилгаа</th>
-          <td>{{ currency(amount * 0.15, 2) }}</td>
+          <th scope="row" style="vertical-align: middle;">Урьдчилгаа</th>
+          <td><input class="form-control" type='number' v-model="preAmount" aria-describedby="emailHelp" placeholder="оруулах" /></td>
+        </tr>
+        <tr>
+          <th scope="row" style="vertical-align: middle;">Жилийн хүү</th>
+          <td><input class="form-control" type='number' v-model="yearPercentage" aria-describedby="emailHelp" placeholder="оруулах" /></td>
         </tr>
         <tr>
           <th scope="row">Зээл авах дүн</th>
           <td>{{ currency(amount * 0.85, 2) }}</td>
         </tr>
         <tr>
-          <th scope="row">Жилийн хүү</th>
-          <td>10%</td>
-        </tr>
-        <tr>
-          <th scope="row">Зээл буцаах дүн</th>
-          <td>{{ currency((amount * 0.85) * (year * 0.1), 2) }}</td>
-        </tr>
-        <tr>
           <th scope="row">Сар бүр төлөх дүн</th>
-          <td><p class="font-weight-bold">{{ currency(((amount * 0.85) * (year * 0.1)) / year / 12) }}</p></td>
+          <td><p class="font-weight-bold">{{ currency(((amount * 0.85) * (year * (yearPercentage / 100))) / year / 12) }}</p></td>
         </tr>
       </tbody>
     </table>
@@ -51,19 +47,27 @@
         data() {
           return {
             year: 15,
+            yearPercentage: 10,
+            tempAmount: 0,
+            preAmount: 0
           }
         },
         methods: {
             onChange: function(event) {
               this.year = event.target.value
             },
+            onAmountChange: function(event) {
+              this.preAmount = parseInt(this.amount * 0.15)
+            },
             currency: function(amount) {
               return (parseFloat(amount)).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')
             }
         },
         mounted() {
-            console.log(this.amount)
-            console.log('Component ready.')
+            this.preAmount = parseInt(this.amount * 0.15)
+            //console.log(this.amount)
+            //this.tempAmount = this.amount
+            //console.log('Component ready.')
         }
     }
 </script>
